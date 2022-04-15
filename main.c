2e671beb9abee1cb9b8054c4800e892bf6995f4e
Dennis Lövkvist <dennis.lovkvist@protonmail.com>
2022-04-16 00:03:24 +0200

    experimenting with cropping

--- main.c
+++ main.c
@@ -23,8 +23,8 @@ int main(void)
     
     input_image.local_buffer = stbi_load(input_path, &input_image.width, &input_image.height, &input_image.bytes_per_pixel, 0);
 
-    output_image.width = input_image.width / 2;
-    output_image.height = input_image.height / 2;
+    output_image.width = input_image.width / 4;
+    output_image.height = input_image.height / 4;
     output_image.local_buffer = (unsigned char *) malloc(output_image.width * output_image.height * input_image.bytes_per_pixel);    
     
     stbir_resize(input_image.local_buffer, 
@@ -43,10 +43,33 @@ int main(void)
                  STBIR_FILTER_BOX, STBIR_FILTER_BOX,
                  STBIR_COLORSPACE_SRGB, NULL);
     
-    stbi_write_png(output_path, output_image.width, output_image.height, input_image.bytes_per_pixel, output_image.local_buffer, 0);
+    //stbi_write_png(output_path, output_image.width, output_image.height, input_image.bytes_per_pixel, output_image.local_buffer, 0);        
+    
+    
+    Image cropped_image;
+    cropped_image.width = 500;
+    cropped_image.height = 500;
+    cropped_image.local_buffer = (unsigned char *) malloc(cropped_image.width * cropped_image.height * input_image.bytes_per_pixel); 
 
+    
+
+
+    for (int y = 0; y < cropped_image.height; y++)
+    {
+        int row = y*cropped_image.width*input_image.bytes_per_pixel;
+        int row2 = y*output_image.width*input_image.bytes_per_pixel;
+        memcpy(&cropped_image.local_buffer[row],&output_image.local_buffer[row2],cropped_image.width*input_image.bytes_per_pixel*sizeof(unsigned char));
+    }
+    
+
+    
+
+    stbi_write_png(output_path, cropped_image.width, cropped_image.height, input_image.bytes_per_pixel, cropped_image.local_buffer, 0);
+    
+    
+    stbi_image_free(cropped_image.local_buffer);
     stbi_image_free(input_image.local_buffer);
     stbi_image_free(output_image.local_buffer);
-    
+
     return 0;
 }
