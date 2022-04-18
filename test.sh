68b0eee38ece8cf3026a175ad672e77a704a6841
Dennis Lövkvist <dennis.lovkvist@protonmail.com>
2022-04-19 01:51:19 +0200

    Fixed test script.

--- test.sh
+++ test.sh
@@ -1,27 +1,30 @@
 #!/bin/sh
-IMAGES=$(ls *.png)
-
-for IMAGE in $IMAGES
+for IMAGE in *.png;
 do
     FILE_INFO=$(identify -format "%wx%h" $IMAGE)
     IFS='x'
     read -a DIMENSIONS <<< "$FILE_INFO"
     WIDTH=${DIMENSIONS[0]}
     HEIGHT=${DIMENSIONS[1]}
+	N=300
 
-    echo $WIDTH
-    echo $HEIGHT
+    if [ $WIDTH -gt $HEIGHT ]
+    then
+        FACTOR=$(echo "$N / $HEIGHT" | bc -l) 
+    else
+        FACTOR=$(echo "$N / $WIDTH" | bc -l)
+    fi
 
-    N=300
-    FACTOR=$(echo "$N / $HEIGHT" | bc -l)
+	delo_img -op resize -i $IMAGE -s $FACTOR
+	
 
-    delo_img -op resize -i $IMAGE -s $FACTOR
+    
+    
 
 done
 
-IMAGES=$(ls resized_*)
 
-for IMAGE in $IMAGES
+for IMAGE in resized_*;
 do
     FILE_INFO=$(identify -format "%wx%h" $IMAGE)
     IFS='x'
@@ -29,13 +32,23 @@ do
     WIDTH=${DIMENSIONS[0]}
     HEIGHT=${DIMENSIONS[1]}
 
-    echo $WIDTH
-    echo $HEIGHT
-
     N=300
-    FACTOR=$(echo "$N / $WIDTH" | bc -l)
+	
+    if [ $WIDTH -ne $HEIGHT ]
+    then
 
-    delo_img -op crop -i $IMAGE -u pct -x 0.25 -y 0 -w $FACTOR -h 1
+        if [ $WIDTH -gt $HEIGHT ]
+        then
+            FACTOR=$(echo "$N / $WIDTH" | bc -l)
+            delo_img -op crop -i $IMAGE -u pct -x 0 -y 0 -w $FACTOR -h 1
+        else
+            FACTOR=$(echo "$N / $HEIGHT" | bc -l)
+            delo_img -op crop -i $IMAGE -u pct -x 0 -y 0 -w 1 -h $FACTOR
+        fi  
+    else
+        delo_img -op crop -i $IMAGE -u pct -x 0 -y 0 -w 1 -h 1
+    fi 
+	
     rm $IMAGE
 
 done
